88ef52c879aafd73ec3c249f3f06327526f72b0f
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-02-26 09:28:46 -0300

    Nesta versão o programa fala todas as cidades do litoral sul de São Paulo em linhas diferentes

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Estas são as cidades que pertencem ao litoral sul de São Paulo" + Environment.NewLine + "Cananéia" +Environment.NewLine+ "Iguape" +Environment.NewLine+ "Ilha Comprida" + Environment.NewLine + "Itanhaém" + Environment.NewLine + "Mongaguá" + Environment.NewLine + "Peruíbe");
+
